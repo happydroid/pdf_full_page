967ca449dcc538115289faf61546c19da0982adc
Artyom Andreyuk <artsiom@threatrix.io>
2024-04-23 14:59:36 +0200

    first commit

---
+++ src/main/java/com/example/pdfgenerator/PdfGenerator.java
@@ -0,0 +1,20 @@
+package com.example.pdfgenerator;
+
+import org.thymeleaf.context.Context;
+
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PdfGenerator {
+
+    public static void main(String[] args) throws Exception {
+        String template = DataToHtmlGenerator.loadTemplateAndGenerateHtml("report", new Context());
+
+        OutputStream outputStream = Files.newOutputStream(Paths.get("output.pdf"));
+        HtmlToPdfRender.generateToOutputStream(List.of(template), outputStream);
+        outputStream.close();
+    }
+
+}
